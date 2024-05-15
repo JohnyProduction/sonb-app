aa51bec73e90c5b1d18fcaad4b2c8b455dff4ead
PJWORKs <pjan319@gmail.com>
2024-05-15 13:17:08 +0200

    Create login register end (#1)
    * create login and register endpoints
    
    * change port to 3333

--- server/index.js
+++ server/index.js
@@ -1,11 +1,39 @@
-var express = require("express");
-var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+let users = [];
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    if (users.find(user => user.username === username)) {
+        return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    users.push({ username, password });
+    res.status(201).json({ message: 'User registered successfully' });
 });
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    res.json({ message: 'Login successful' });
+});
+
+const PORT = 3333;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 
 /*
 -write login api
